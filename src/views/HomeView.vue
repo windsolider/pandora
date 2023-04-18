84ec781a343954212f0fc1ad1e18e681e8cac8b6
taocan6 <taocan6@jd.com>
2023-04-18 17:10:05 +0800

    安装sass和引入bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+//import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <div class="sidebar">
+      <ul></ul>
+    </div>
+    <div class="dialog"></div>
+  </div>
 </template>
+<style scoped>
+.sidebar {
+  width: 240px;
+  position: fixed;
+  left:0;
+  top:0;
+  background-color:black;
+}
+</style>
